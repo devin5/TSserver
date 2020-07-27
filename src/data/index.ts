d458f3b26acd9f90fe9867ec4623918b209efe3a
Devin <devindias.tech@gmail.com>
2020-07-27 09:35:55 -0500

    fixes formatting

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,16 +1,17 @@
 import 'dotenv/config'
 import mongoose, { Mongoose } from 'mongoose'
-import Users from './models/usersModel'
+import Users from "./models/usersModel"
 
 const connectDb = (): Promise<Mongoose> => {
-    return mongoose.connect(process.env.DATABASE_URL!, {
+          return mongoose.connect(process.env.DATABASE_URL!, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
 }
 
 const models = { Users }
+// practice
 
 export { connectDb }
 
-export default models
+        export default models
